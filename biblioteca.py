ede5785681157d09b92df902f84093dec1e96c8a
oscarbprofe <oscar.badilla@profesor.duoc.cl>
2025-06-14 19:14:05 -0400

    crea archivo princial y listas libros y personas

---
+++ biblioteca.py
@@ -0,0 +1,25 @@
+libros = [  
+    {"id": 1, "titulo": "Cien años de soledad", "autor": "Gabriel García Márquez", "ISBN": "978-0307474728", "paginas": 432, "cantidad_disponible": 5},
+    {"id": 2, "titulo": "1984", "autor": "George Orwell", "ISBN": "978-0451524935", "paginas": 328, "cantidad_disponible": 3},
+    {"id": 3, "titulo": "Fahrenheit 451", "autor": "Ray Bradbury", "ISBN": "978-1451673319", "paginas": 194, "cantidad_disponible": 7},
+    {"id": 4, "titulo": "Don Quijote", "autor": "Miguel de Cervantes", "ISBN": "978-0060934347", "paginas": 992, "cantidad_disponible": 2},
+    {"id": 5, "titulo": "Crónica de una muerte anunciada", "autor": "Gabriel García Márquez", "ISBN": "978-1400034956", "paginas": 128, "cantidad_disponible": 4},
+    {"id": 6, "titulo": "El Principito", "autor": "Antoine de Saint-Exupéry", "ISBN": "978-0156013987", "paginas": 96, "cantidad_disponible": 10},
+    {"id": 7, "titulo": "Ensayo sobre la ceguera", "autor": "José Saramago", "ISBN": "978-0156007757", "paginas": 352, "cantidad_disponible": 3},
+    {"id": 8, "titulo": "La sombra del viento", "autor": "Carlos Ruiz Zafón", "ISBN": "978-0143034902", "paginas": 512, "cantidad_disponible": 6},
+    {"id": 9, "titulo": "El túnel", "autor": "Ernesto Sabato", "ISBN": "978-9500420305", "paginas": 160, "cantidad_disponible": 2},
+    {"id": 10, "titulo": "Pedro Páramo", "autor": "Juan Rulfo", "ISBN": "978-6073142360", "paginas": 144, "cantidad_disponible": 8}
+]
+
+personas = [
+    {"nombre": "Ana", "apellido": "González", "rut": "13816108-7", "libros": [3, 2, 6]},
+    {"nombre": "Luis", "apellido": "Rodríguez", "rut": "13872719-2", "libros": [1, 8, 10, 4, 7]},
+    {"nombre": "Camila", "apellido": "Pérez", "rut": "12182343-5", "libros": [5, 8, 2]},
+    {"nombre": "Jorge", "apellido": "Muñoz", "rut": "14044461-9", "libros": [7, 9, 4, 6, 8]},
+    {"nombre": "María", "apellido": "Rojas", "rut": "16149391-0", "libros": [2, 8, 7]},
+    {"nombre": "Diego", "apellido": "Díaz", "rut": "10407062-4", "libros": [3, 2, 10]},
+    {"nombre": "Lucía", "apellido": "Soto", "rut": "19306158-3", "libros": [4, 1, 10]},
+    {"nombre": "Pablo", "apellido": "Torres", "rut": "14864522-5", "libros": [9, 6]},
+    {"nombre": "Valentina", "apellido": "Contreras", "rut": "15592214-1", "libros": [4, 10, 8, 7]},
+    {"nombre": "Tomás", "apellido": "Silva", "rut": "10516040-5", "libros": [1, 7, 10]}
+]
